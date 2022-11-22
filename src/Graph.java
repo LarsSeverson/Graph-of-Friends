9f167575c775ab99c81a2c4b5cf893578be7d99d
Lars Severson <larsseverson1@gmail.com>
2022-11-21 23:08:00 -0600

    Hash Map Adjacency List

--- src/Graph.java
+++ src/Graph.java
@@ -1,39 +1,58 @@
 import java.util.*;
 
 public class Graph{
-    private final List<Node>[] Students;
-
+    private final Map<Vertex, List<Vertex>> adjList;
     private int numberOfVertices;
+    private int numberOfEdges;
+    public Graph(){
+        numberOfVertices = 0;
+        numberOfEdges = 0;
+        adjList = new HashMap<>();
+    }
 
-    public Graph(int size){
-        // initial number of vertex
-        numberOfVertices = size;
-        Students = new LinkedList[size];
-        for(int i = 0; i < Students.length; i++){
-            Students[i] = new LinkedList<>();
+    public void addVertex(String[] data){
+        //                             ID       fName    lName    cName    dName    email    friendCount
+        adjList.putIfAbsent(new Vertex(data[0], data[1], data[2], data[3], data[4], data[5], data[6]), new ArrayList<>());
+        numberOfVertices++;
+    }
+
+    public void addEdge(String sourceID, String friendID){
+        Vertex source = find(sourceID);
+        Vertex friend = find(friendID);
+
+        adjList.get(source).add(friend);
+        adjList.get(friend).add(source);
+
+        numberOfEdges++;
+    }
+    public int getNumberOfVertices() {
+        return numberOfVertices;
+    }
+    public int getNumberOfEdges() {
+        return numberOfEdges;
+    }
+
+    private Vertex find(String id){
+        for(Vertex i : adjList.keySet()){
+            if (i.studentId.equals(id)){
+                return i;
+            }
         }
-
+        return null;
     }
 
-    private static class Edge{
+    private static class Vertex {
+        private final String studentId;
+        private final String friendCount;
+        private final String firstName;
+        private final String lastName;
+        private final String collegeName;
+        private final String departmentName;
+        private final String studentEmail;
 
-    }
-    private static class Node{
-        private int studentId;
-        private int friendCount;
-
-        private String firstName;
-        private String lastName;
-        private String collegeName;
-        private String departmentName;
-        private String studentEmail;
-
-        private ArrayList<Integer> friends;
-
-        private Node(int studentId, String firstName, String lastName, String collegeName, String departmentName, String studentEmail, int friendCount){
+        private Vertex(String studentId, String firstName, String lastName, String collegeName, String departmentName, String studentEmail, String friendCount){
             this.studentId = studentId; this.firstName = firstName; this.lastName = lastName; this.collegeName = collegeName;
             this.departmentName = departmentName; this.studentEmail = studentEmail; this.friendCount = friendCount;
         }
-
     }
 }
--- src/GraphDriver.java
+++ src/GraphDriver.java
@@ -1,21 +1,73 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class GraphDriver {
     private static Graph theGraph;
+    private static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
-        int size = -1;
+        theGraph = new Graph();
+        System.out.print("Enter file name: ");
+        // Assignment05_inputFile.txt
         try{
-            BufferedReader theFile = new BufferedReader(new FileReader("src/Assignment05_inputFile.txt"));
-            while(theFile.readLine()!=null){
-                size++;
-            }
+            String in;
+            new BufferedReader(new FileReader(in = input.nextLine()));
+            System.out.println("\nInput file is read successfully..");
+
+            // Vertex
+            setVertex(new BufferedReader(new FileReader(in)));
+            // Edges
+            setEdge(new BufferedReader(new FileReader(in)));
+
+            System.out.println("Total number of vertices in the Graph: " + theGraph.getNumberOfVertices());
+            System.out.println("Total number of edges in the Graph: " + theGraph.getNumberOfEdges());
+
+            //while(true){
+            System.out.println("\n1. Remove friendship\n" +
+                    "2. Delete Account\n" +
+                    "3. Count friends\n" +
+                    "4. Friends Circle\n" +
+                    "5. Closeness centrality\n" +
+                    "6. Exit\n");
+
+
         }
         catch(IOException e){
-            e.printStackTrace();
+            System.out.println("File not found");
         }
+    }
+    private static void setEdge(BufferedReader theFile) throws IOException{
+        String line;
+        int i = 0;
 
-        theGraph = new Graph(size);
+        // Add edge
+        while((line = theFile.readLine()) != null){
+            if (i > 0){
+                String[] data = line.split("\t");
+                if (data.length > 7){
+                    int index = 7;
+                    while(index < data.length){
+                        // Add edge
+                        theGraph.addEdge(data[0], data[index]);
+                        index++;
+                    }
+                }
+            }
+            i++;
+        }
+    }
+    private static void setVertex(BufferedReader theFile) throws IOException {
+        String line;
+        int i = 0;
+
+        // Add vertices
+        while((line = theFile.readLine()) != null){
+            if (i > 0){
+                String[] data = line.split("\t");
+                theGraph.addVertex(data);
+            }
+            i++;
+        }
     }
 }
